e53b09cf1b66bafa238aeaad549aa4a92592ebad
Cokoino <37071399+Cokoino@users.noreply.github.com>
2019-05-03 23:33:21 +0800

    Add files via upload

---
+++ Active_buzzer_code/Active_buzzer_code.ino
@@ -0,0 +1,10 @@
+int buzzPin = 3;                   //Define the number port 3
+void setup(){
+    pinMode(buzzPin, OUTPUT);    //Set buzzPin to output mode
+}
+void loop(){
+    digitalWrite(buzzPin, HIGH); //The active buzzer sounds
+    delay(2000);                 //delay 2S
+    digitalWrite(buzzPin, LOW);  //Active buzzer off
+    delay(2000);                 //delay 2S      
+}
